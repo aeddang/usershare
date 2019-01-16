3de3ec171c83bbef34199c46da71a3c31086b457
aeddang <aeddang@kakaovx.com>
2019-01-16 15:46:50 +0900

    update

--- app/src/main/java/com/kakaovx/homet/component/network/ApiComponent.kt
+++ app/src/main/java/com/kakaovx/homet/component/network/ApiComponent.kt
@@ -1,16 +1,13 @@
 package com.kakaovx.homet.component.network
-import android.support.v4.app.Fragment
 import android.view.View
 import com.kakaovx.homet.component.annotation.PageScope
-import com.kakaovx.homet.component.annotation.UserScope
 import com.kakaovx.homet.component.app.AppComponent
-import com.kakaovx.homet.component.network.api.GitHubApi
-import com.kakaovx.homet.page.PageNetworkTest
+import com.kakaovx.homet.component.ui.ApiPageFragment
 import dagger.Component
 
 @PageScope
 @Component(dependencies = [AppComponent::class], modules = [ApiModule::class])
 interface ApiComponent {
     fun inject(context: View)
-    fun inject(context: PageNetworkTest)
+    fun inject(context: ApiPageFragment)
 }
---
+++ app/src/main/java/com/kakaovx/homet/component/ui/ApiPageFragment.kt
@@ -0,0 +1,25 @@
+package com.kakaovx.homet.component.page
+
+import android.support.v4.app.Fragment
+import com.kakaovx.homet.App
+import com.kakaovx.homet.component.network.ApiModule
+import com.kakaovx.homet.component.network.DaggerApiComponent
+import com.kakaovx.homet.component.network.api.GitHubApi
+import javax.inject.Inject
+
+abstract class ApiPageFragment : InjectablePageFragment()
+{
+    @Inject
+    lateinit var api: GitHubApi
+
+    override fun inject(fragment: Fragment) {
+        fragment.context?.run {
+            DaggerApiComponent.builder()
+                .appComponent(App.getAppComponent(this)).apiModule(ApiModule())
+                .build().inject(this@ApiPageFragment)
+        }
+    }
+
+    abstract protected fun handleComplete(data: Any)
+    abstract protected fun handleError(err:Throwable)
+}
