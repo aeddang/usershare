1a0c9b996bcdde25542664b06d5df7462cb14938
ricky <aeddang@kakaovx.com>
2019-04-05 18:49:41 +0900

    fix

--- app/src/main/java/com/kakaovx/homet/user/component/preference/PreferenceName.kt
+++ app/src/main/java/com/kakaovx/homet/user/component/preference/PreferenceName.kt
@@ -3,6 +3,6 @@ package com.kakaovx.homet.user.component.preference
 class PreferenceName {
     companion object {
         const val SETTING: String = "pref_setting"
-        const val LOGIN: String = "pref_login_v02"
+        const val LOGIN: String = "pref_login_v03"
     }
 }
--- app/src/main/java/com/kakaovx/homet/user/component/ui/view/gif/Gif.kt
+++ app/src/main/java/com/kakaovx/homet/user/component/ui/view/gif/Gif.kt
@@ -1,3 +1,35 @@
 package com.kakaovx.homet.user.component.ui.view.gif
 
-class Gif
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import com.kakaovx.homet.user.component.ui.skeleton.view.AnimatedDrawView
+import com.kakaovx.homet.user.component.ui.view.graph.GraphUtil
+
+class Gif@kotlin.jvm.JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
+    :  AnimatedDrawView(context, attrs, defStyleAttr) {
+    private val TAG = javaClass.simpleName
+
+    fun initSet(totalAmount:Double, color:String,  isVertical:Boolean = true) {
+
+    }
+
+    @SuppressLint("DrawAllocation")
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+    }
+
+    override fun onStart() {
+
+    }
+
+    override fun onCompute(f: Int) {
+
+    }
+
+    override fun onCompleted(f: Int) {
+
+    }
+}
