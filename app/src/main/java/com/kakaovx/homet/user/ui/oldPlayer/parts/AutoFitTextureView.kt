5f108d268622856ad1b570bc2002e2ab9e1a9dfd
peter <parkkw09@kakaovx.com>
2019-04-08 14:48:09 +0900

    refactoring view
    - move autofittextureview for oldplayer

--- app/src/main/java/com/kakaovx/homet/user/ui/oldPlayer/parts/AutoFitTextureView.kt
+++ app/src/main/java/com/kakaovx/homet/user/ui/oldPlayer/parts/AutoFitTextureView.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.kakaovx.homet.user.component.ui.view
+package com.kakaovx.homet.user.ui.oldPlayer.parts
 
 import android.content.Context
 import android.util.AttributeSet
